2b87de9dd05d81a400bcf828b0021c6339707be0
John <jontedev@gmail.com>
2020-04-11 02:54:57 +0300

    Errorlog channel driver specified

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,17 +7,20 @@ use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * The trusted proxies for this application.
-     *
-     * @var array|string
-     */
-    protected $proxies;
+// /**
+    //  * The trusted proxies for this application.
+    //*
+    //* @var array|string
+    //*/
+    // protected $proxies;
 
-    /**
-     * The headers that should be used to detect proxies.
-     *
-     * @var int
-     */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+// /**
+    //  * The headers that should be used to detect proxies.
+    //  *
+    //  * @var int
+    //  */
+    // protected $headers = Request::HEADER_X_FORWARDED_ALL;
+
+    protected $proxies = '*';
+    protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
 }
