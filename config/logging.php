e3c24ac7023a668fdafd2387d498b78eee3e95a0
John <jontedev@gmail.com>
2020-04-11 02:34:19 +0300

    Errorlog channel driver revert

--- config/logging.php
+++ config/logging.php
@@ -42,8 +42,8 @@ return [
         ],
 
         'single' => [
-            'driver' => 'errorlog',
-            // 'path' => storage_path('logs/laravel.log'),
+            'driver' => 'single',
+            'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],
 
