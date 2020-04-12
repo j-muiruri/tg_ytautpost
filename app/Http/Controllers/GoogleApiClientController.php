41f4745740c726fc08f5cf566a4f422c6d49c242
John <jontedev@gmail.com>
2020-04-12 13:47:40 +0300

    troubleshoot production --- redirect error

--- app/Http/Controllers/GoogleApiClientController.php
+++ app/Http/Controllers/GoogleApiClientController.php
@@ -6,47 +6,51 @@ use Illuminate\Http\Request;
 use Google_Client;
 use Google_Service_YouTube;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Redirect;
 
 class GoogleApiClientController extends Controller
 {
-      /**
+    /**
      * Perform Authentication
      */
-    public function getAuthGoogleApi() {
+    public function getAuthGoogleApi()
+    {
 
         $client = new Google_Client();
 
-        
-        $client->setApplicationName('Autopost Telegram Bot'); 
-        
+
+        $client->setApplicationName('Autopost Telegram Bot');
+
         $client->setScopes(['https://www.googleapis.com/auth/youtube.readonly',]);
-        
+
         //$client->setAuthConfig(env('CLIENT_SECRET')); 
 
         $client->setClientId(env('CLIENT_ID'));
-        
+
         $client->setClientSecret(env('CLIENT_SECRET_PASS'));
 
         $client->setDeveloperKey(env('YOUTUBE_API_KEY', 'YOUR_API_KEY'));
-        
-        $client->setAccessType('offline'); 
 
-        //Redirect PAth or URL
-        //$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-        $redirect_uri = Redirect::route('my-auth');
-        
+        $client->setAccessType('offline');
+
+        if (App::environment() === 'production') {
+            $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+        } else {
+            //Redirect PAth or URL
+            $redirect_uri = 'http://'. $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+        }
         $client->setRedirectUri($redirect_uri);
-       
+
         $url = $client->createAuthUrl();
 
         // Exchange authorization code for an access token.
-         
+
         if (isset($_GET['code'])) {
 
             $accessToken = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-            
-            $client->setAccessToken($accessToken); 
+
+            $client->setAccessToken($accessToken);
 
             return response()->json($accessToken);
         }
@@ -56,11 +60,12 @@ class GoogleApiClientController extends Controller
     /**
      * Get Playlists
      */
-    public function getPlaylists() {
+    public function getPlaylists()
+    {
         // Define service object for making API requests.
-        
+
         // $access_token = $request;
-        
+
         $client = new Google_Client();
 
         // $httpClient = new Client([
@@ -69,16 +74,16 @@ class GoogleApiClientController extends Controller
         //     ]
         // ]);
         // $client->setHttpClient($httpClient);
-   
-        
+
+
         $service = new Google_Service_YouTube($client);
 
         $queryParams = [
-            'maxResults' => 25, 
+            'maxResults' => 25,
             'mine' => true
-            ]; 
+        ];
 
-        $response = $service->playlists->listPlaylists('snippet,contentDetails', $queryParams); 
+        $response = $service->playlists->listPlaylists('snippet,contentDetails', $queryParams);
 
         return response()->json($response);
     }
