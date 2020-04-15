23e45e2126b55437108e3af93bff3753079aa93b
John <jontedev@gmail.com>
2020-04-16 01:14:58 +0300

    Added TelegramBot, YoutubeVideos Model and Start Cpmmand

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -2,18 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Telegram;
+// use Telegram;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
+/**
+ * The Telegram Bot  Class                                         
+ *+ 
+ * @author John Muiruri  <jontedev@gmail.com>
+ * 
+ */
 class TelegramBotController extends Controller
 {
     /**
-     * Get Updates(Messages from users or input)
+     * Get Updates(Messages from users or input) via Long polling
+     * Cant work if a webhook is already setup
      */
     public function updatedActivity()
     {
         $activity = Telegram::getUpdates();
         return response()->json($activity);
     }
+
     /**
      * Run Commands (eg. /start, /help)
      */
@@ -22,14 +31,14 @@ class TelegramBotController extends Controller
         $update = Telegram::commandsHandler(false, ['timeout' => 0]);
         return response()->json(['status' => 'success']);
     }
+
     /**
-     * Register Webhok (eg. /start, /help)
+     * Register Webhook (eg. /start, /help)
      */
     public function tgWebhook()
     {
         $response = $update = Telegram::commandsHandler(true);
         return response()->json(['status' => 'success']);
-        
     }
 
     /**
@@ -37,15 +46,17 @@ class TelegramBotController extends Controller
      */
     public function runWebhook()
     {
-            $url = 'https://tgytautopost.herokuapp.com/'.env('TELEGRAM_BOT_TOKEN') .'/webhook';
-            $updates = Telegram::setWebhook(['url' => $url]);
-            return response()->json($updates);
+        $url = 'https://tgytautopost.herokuapp.com/' . env('TELEGRAm_BOT_TOKEN') . '/webhook';
+        $updates = Telegram::setWebhook(['url' => $url]);
+        return response()->json($updates);
     }
 
     /**
      * Remove Webhook
      */
-    public function removeWebhook(){
+    public function removeWebhook()
+    {
         $response = Telegram::removeWebhook();
+        return response()->json($response);
     }
 }
