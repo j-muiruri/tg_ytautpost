86fe2d4b8cef378bb8e747b4b7f5f94a907ff42b
John <jontedev@gmail.com>
2020-09-26 11:49:47 +0300

    Passport Install

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
