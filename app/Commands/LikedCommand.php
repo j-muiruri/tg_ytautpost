0aa47049091bf8d9fe25e704d32a2ab715630665
John <jontedev@gmail.com>
2020-10-07 16:42:40 +0300

    Too many requests on MyLiked command, trying sleep

--- app/Commands/LikedCommand.php
+++ app/Commands/LikedCommand.php
@@ -52,7 +52,7 @@ class LikedCommand extends Command
             $no++;
 
             $this->replyWithMessage(['text' => $no.'. '.$title.' - '.$link]);
-            usleep(2400000); //2.8 secs
+            usleep(1900000); //2.8 secs
         }
         // send next page link
 
