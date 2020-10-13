0cc03fd689953da6216c27534d6ab8fcf38202da
John <jontedev@gmail.com>
2020-10-13 10:49:34 +0300

    Generating tokens, previous command get message from object result

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -155,7 +155,7 @@ class TelegramBotController extends Controller
                 ['message_type', '=', 'bot_command'],
             ])->orderBy('id', 'desc')
             ->limit(1);
-        $message = $command["message"];
+        $message = (array) $command;
         Log::debug($message);
         $commandDetails = array();
         $commandDetails["message"] = $message;
