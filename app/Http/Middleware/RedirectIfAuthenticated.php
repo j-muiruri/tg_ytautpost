b5655b05c47305081307923cd505210993eca296
John <jontedev@gmail.com>
2020-11-22 13:43:21 +0300

    Added redirect option to login

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,10 +18,17 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
-        }
+    //     if (Auth::guard($guard)->check()) {
+    //         return redirect(RouteServiceProvider::HOME);
+    //     }
 
+
+    //     return $next($request);
+    // }
+
+        if (Auth::guard($guard)->check()) {
+            return redirect()->intended('/');
+        }
+        
         return $next($request);
     }
-}
